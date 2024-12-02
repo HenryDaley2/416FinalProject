a5fd0d62be562763ba679e8ec04056a8f35fa7d7
ethanHomiski <ethan.homiski@hotmail.com>
2024-12-02 18:17:12 -0500

    CreateAccount form

--- Server/Server.js
+++ Server/Server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, '../client')));
 
 // Serve the home page or portfolio page based on the routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client', 'login.html'));
+    res.sendFile(path.join(__dirname, '../client', 'FrontEnd.html'));
 });
 
 // Create
